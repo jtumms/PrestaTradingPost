f4d5a87f82b0152a775266e4a7a0fa12d1851051
jtumms <john.tumminelli@tummsmedia.com>
2016-11-21 22:25:45 -0500

    Added image upload provisions to /add-item

--- src/main/java/com/tummsmedia/controllers/ImageUploadController.java
+++ src/main/java/com/tummsmedia/controllers/ImageUploadController.java
@@ -1,11 +1,36 @@
 package com.tummsmedia.controllers;
 
+import com.tummsmedia.entities.Image;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by john.tumminelli on 11/21/16.
  */
+@Controller
 public class ImageUploadController {
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String upload(MultipartFile file) throws IOException {
 
+        File dir = new File("public/files");
+        dir.mkdirs();
+        StringBuilder sb = new StringBuilder();
+
+        File f = File.createTempFile("file", file.getOriginalFilename(), dir);
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(file.getBytes());
+
+        Image itemImage = new Image(f.getName(), file.getOriginalFilename());
+        files.save(anonFile);
+
+
+    }
 
 }
